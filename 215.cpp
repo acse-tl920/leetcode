0732a17ad53f24cd3fdd638be346afa1f926dd15
acse-tl920 <tl920@ic.ac.uk>
2021-05-16 18:14:17 +0100

    add 215

---
+++ 215.cpp
@@ -0,0 +1,22 @@
+#include "main.h"
+
+using namespace std;
+
+class Solution{
+public:
+    int findKthLargest(vector<int> & nums, int k){
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+
+        int n = nums.size();
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            minHeap.push(nums[i]);
+            if(minHeap.size() > k)
+            {
+                minHeap.pop();
+            }
+        }
+        return minHeap.top();
+    }
+};
